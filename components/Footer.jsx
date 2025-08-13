3e9cc0b94a45c16fb74b6037e884d3d262f25f7d
nzmshrxz <nazamshiraz4@gmail.com>
2025-08-13 18:10:41 +0530

    minor fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className=" mt-10 bg-[#000000a5] dark:text-white py-6 text-center">
+    <footer className=" mt-10 bg-[#000000a5] text-white py-6 text-center">
       <p className="text-sm">© 2025 BulletinNews. All rights reserved.</p>
       <p className="text-sm">© Nazam shiraz.</p>
     </footer>
